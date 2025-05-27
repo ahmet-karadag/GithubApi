53596cf0b1d72b72bd923c4d04a7b8107e613d55
Ahmet Karadağ <akkaradaga@gmail.com>
2025-05-27 23:41:40 +0300

    Initial Commit

---
+++ UserGithubApi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  UserGithubApi
+//
+//  Created by ahmet karadağ on 27.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ UserGithubApi/UserGithubApiApp.swift
@@ -0,0 +1,17 @@
+//
+//  UserGithubApiApp.swift
+//  UserGithubApi
+//
+//  Created by ahmet karadağ on 27.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct UserGithubApiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
